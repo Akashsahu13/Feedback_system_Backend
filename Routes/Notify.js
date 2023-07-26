0d36f2ce6167d42a9d26efaca62f1751c9afd449
Akash sahu <110984597+Akashsahu13@users.noreply.github.com>
2023-07-26 19:43:00 +0530

    Update Notify.js

--- Routes/Notify.js
+++ Routes/Notify.js
@@ -4,7 +4,7 @@ const Notify=(io)=>{
 io.on("connect", (socket)=>{
 console.log(10);
 socket.on('send-name',(name)=>{
-    console.log(name);
+    console.log("inside socket io");
     console.log(`line${8} inside notify`)
     console.log("count")
     io.emit("Notification", name);
